08e71b81efd1f15690e55c15f9392a6982d86d70
Chris Xiao <csx773@gmail.com>
2019-05-14 22:07:09 +0000

    finished bonus store validation

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,13 @@ class Store < ActiveRecord::Base
 
     validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0} 
     validates :name, length: { minimum: 3 }
+    validate :have_atleast_one_appreal
+
+    def have_atleast_one_appreal
+        unless mens_apparel or womens_apparel
+            errors.add(:mens_apparel, "Must have atleast men appreal")
+            errors.add(:womens_apparel, "Must have atleast women appreal")
+        end
+    end
 end
+
