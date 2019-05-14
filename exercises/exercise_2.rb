7ea00ceef04513f4bef173fe27e49a5d1eb48127
Chris Xiao <csx773@gmail.com>
2019-05-14 19:52:56 +0000

    exercise 2 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,19 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# david = User.find_by(name: 'David')
+
+# burnaby store
+@store1 = Store.find_by(id: 1);
+# richmond store
+@store2 = Store.find_by(id: 2);
+
+puts "Store 1 name: #{@store1.name}"
+puts "Store 2 name: #{@store2.name}"
+
+@store1.name = 'Vancouver'
+@store1.save
+
+puts "Store 1 NEW name: #{@store1.name}"
+puts "Store 2 name: #{@store2.name}"
+
