6f718f267a08cee2f9437669ece43ddacc7ce800
Chris Xiao <csx773@gmail.com>
2019-05-14 19:45:10 +0000

    exercise 1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,17 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+# user = User.create(name: "David", occupation: "Code Artist")
+
+# * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# * Richmond (annual_revenue of 1260000 carries women's apparel only)
+# * Gastown (annual_revenue of 190000 carries men's apparel only)
+
 # Your code goes below here ...
+
+burnabyStore = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+richmondStore = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+gastownStore = Store.create(name: "Gastown", annual_revenue: 19000, mens_apparel: true, womens_apparel: false )
+
+puts "Number of stores: #{Store.count}"
+
